32ef6e615d853c72d1d0260783efccd905dbbfb9
arfullight <tmimvhuang@gmail.com>
2016-08-19 12:33:37 +0800

    test if all fields will be updated

--- spec/controllers/admin/speakers_controller_spec.rb
+++ spec/controllers/admin/speakers_controller_spec.rb
@@ -19,9 +19,21 @@ RSpec.describe Admin::SpeakersController, type: :controller do
 
   describe 'POST #create' do
     it 'create new record when post' do
+      speaker = build(:speaker)
+
       expect{
-        post :create, speaker: attributes_for(:speaker)
+        post :create, speaker: speaker.attributes
       }.to change(Speaker, :count).by(1)
+
+      # test for every attributes
+      speaker_after = Speaker.last
+
+      # remove auto create fields
+      speaker_after.id = nil
+      speaker_after.updated_at = nil
+      speaker_after.created_at = nil
+
+      expect(speaker_after.attributes).to eq(speaker.attributes)
     end
 
     it 'redirect to admin_speakers_path when update success' do
